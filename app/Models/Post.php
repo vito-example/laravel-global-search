1256f606fef81bb6cab61464a2fcd20338be4bbb
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 14:27:12 +0400

    Add post searchAble scout trait and pass to search fields array, also give index.

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -10,6 +10,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Laravel\Scout\Searchable;
 
 /**
  * Class Post
@@ -23,5 +24,28 @@ use Illuminate\Database\Eloquent\Model;
  */
 class Post extends Model
 {
-    use HasFactory;
+    use HasFactory, Searchable;
+
+    /**
+     *
+     */
+    public const SEARCHABLE_FIELDS = ['id','title','body'];
+
+    /**
+     * @return array
+     */
+    public function toSearchableArray(): array
+    {
+        return $this->only(self::SEARCHABLE_FIELDS);
+    }
+
+    /**
+     * Get the name of the index associated with the model.
+     *
+     * @return string
+     */
+    public function searchableAs()
+    {
+        return 'posts_index';
+    }
 }
