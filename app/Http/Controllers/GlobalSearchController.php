c8861e0fb4f2174c264ac23f8f444366563699da
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 14:29:30 +0400

    Make global search route and GlobalSearchController index method for this route.

---
+++ app/Http/Controllers/GlobalSearchController.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ *  app/Http/Controllers/GlobalSearchController.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 14:28
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+
+/**
+ * Class GlobalSearchController
+ * @package App\Http\Controllers
+ */
+class GlobalSearchController extends Controller
+{
+
+    /**
+     * @param \Illuminate\Http\Request $request
+     */
+    public function index(Request $request) {
+        //
+    }
+}
