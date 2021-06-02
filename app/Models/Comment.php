4de5b8dcc202735954a08723fc36c8c125786b18
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 13:47:54 +0400

    Add comment model.

---
+++ app/Models/Comment.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ *  app/Models/Comment.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 13:45
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+/**
+ * Class Comment
+ * @package App\Models
+ * @property integer $id
+ * @property integer $user_id
+ * @property integer $post_id
+ * @property string $body
+ * @property string $created_at
+ * @property string $updated_at
+ */
+class Comment extends Model
+{
+    use HasFactory;
+}
