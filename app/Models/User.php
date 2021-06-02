45b8d973ba30cee0f30091b713ad26b9035f3575
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 13:17:06 +0400

    Give annotations to user model.

--- app/Models/User.php
+++ app/Models/User.php
@@ -1,5 +1,11 @@
 <?php
-
+/**
+ *  app/Models/User.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 13:16
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
 namespace App\Models;
 
 use Illuminate\Contracts\Auth\MustVerifyEmail;
@@ -7,6 +13,18 @@ use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
 use Illuminate\Notifications\Notifiable;
 
+/**
+ * Class User
+ * @package App\Models
+ * @property integer $id
+ * @property string $name
+ * @property string|null $email
+ * @property string|null $email_verified_at
+ * @property string $password
+ * @property string $remember_token
+ * @property string $created_at
+ * @property string $updated_at
+ */
 class User extends Authenticatable
 {
     use HasFactory, Notifiable;
