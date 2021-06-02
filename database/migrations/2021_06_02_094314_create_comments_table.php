1dd13a0e07bbfaa6719c3a90c5e76ae77f24b041
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 13:45:29 +0400

    Add comments migration.

---
+++ database/migrations/2021_06_02_094314_create_comments_table.php
@@ -0,0 +1,40 @@
+<?php
+/**
+ *  database/migrations/2021_06_02_094314_create_comments_table.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 13:43
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCommentsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('comments', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('user_id')->constrained('users')->onDelete('cascade');
+            $table->foreignId('post_id')->constrained('posts')->onDelete('cascade');
+            $table->text('body');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('comments');
+    }
+}
