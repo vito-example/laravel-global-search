03d369e1531046bd808cb7a823e8bb290424483e
makhatadze <vitomaxatadze@gmail.com>
2021-06-02 14:13:54 +0400

    Add PostFactory.

---
+++ database/factories/PostFactory.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ *  database/factories/PostFactory.php
+ *
+ * Date-Time: 02.06.21
+ * Time: 14:10
+ * @author Vito Makhatadze <vitomaxatadze@gmail.com>
+ */
+namespace Database\Factories;
+
+use App\Models\Post;
+use App\Models\User;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class PostFactory extends Factory
+{
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Post::class;
+
+    /**
+     * Define the model's default state.
+     *
+     * @return array
+     */
+    public function definition(): array
+    {
+        return [
+            'user_id' =>function() {
+                return $this->create(User::class)->id;
+            },
+            'title' => $this->faker->realText(40),
+            'body' => $this->faker->realText(150),
+        ];
+    }
+}
